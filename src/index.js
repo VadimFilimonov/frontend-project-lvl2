944280f49efa484570bbd63560c19db3da78bbd7
Vadim Filimonov <philimonovvadim@gmail.com>
2021-02-05 21:38:26 +0300

    Split gendiff for create ats

--- src/index.js
+++ src/index.js
@@ -1,23 +1,64 @@
 const has = Object.prototype.hasOwnProperty;
 
-const gendiff = (json1, json2) => {
-  const diffs = [];
-
+const gentree = (json1, json2) => {
   const keys = [...Object.keys(json1), ...Object.keys(json2)];
   const sortedKeys = keys.sort();
   const uniqueKeys = [...new Set(sortedKeys)];
 
+  const tree = [];
+
   uniqueKeys.forEach((key) => {
     if (!has.call(json1, key)) {
-      diffs.push(`  + ${key}: ${json2[key]}`);
+      tree.push({
+        name: key,
+        value: json2[key],
+        status: 'added',
+      });
     } else if (!has.call(json2, key)) {
-      diffs.push(`  - ${key}: ${json1[key]}`);
+      tree.push({
+        name: key,
+        value: json1[key],
+        status: 'deleted',
+      });
     } else if (json1[key] !== json2[key]) {
-      diffs.push(`  - ${key}: ${json1[key]}`);
-      diffs.push(`  + ${key}: ${json2[key]}`);
+      tree.push({
+        name: key,
+        value: json2[key],
+        oldValue: json1[key],
+        status: 'changed',
+      });
     } else {
-      diffs.push(`    ${key}: ${json1[key]}`);
+      tree.push({
+        name: key,
+        value: json1[key],
+        status: 'unchanged',
+      });
     }
+  });
+
+  return tree;
+};
+
+const gendiff = (json1, json2) => {
+  const tree = gentree(json1, json2);
+  const diffs = tree.map((node) => {
+    const {
+      name, value, oldValue, status,
+    } = node;
+
+    if (status === 'changed') {
+      const deletedRow = `  - ${name}: ${oldValue}`;
+      const addedRow = `  + ${name}: ${value}`;
+      return `${deletedRow}\n${addedRow}`;
+    }
+    if (status === 'added') {
+      return `  + ${name}: ${value}`;
+    }
+    if (status === 'deleted') {
+      return `  - ${name}: ${value}`;
+    }
+
+    return `    ${name}: ${value}`;
   });
 
   return ['{', ...diffs, '}'].join('\n');
