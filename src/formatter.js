1ee3a959efb9e721a6c508b372c9e45f8341170e
Vadim Filimonov <philimonovvadim@gmail.com>
2021-02-26 21:47:12 +0300

    Transfer stringify to utilities; refactor to guard expression

--- src/formatter.js
+++ src/formatter.js
@@ -1,37 +1,11 @@
+import { stringify } from './utilities.js';
+
 const spaceCount = 4;
 
 const findSymbol = (status) => {
   if (status === 'added') return '+';
   if (status === 'deleted') return '-';
   return ' ';
-};
-
-const stringify = (value, startDepth = 1) => {
-  const iter = (currentValue, depth) => {
-    if (typeof currentValue !== 'object') {
-      return currentValue.toString();
-    }
-    if (currentValue === undefined) {
-      return 'undefined';
-    }
-    if (currentValue === null) {
-      return null;
-    }
-    const indentSize = depth * spaceCount;
-    const currentIndent = ' '.repeat(indentSize);
-    const bracketIndent = ' '.repeat(indentSize - spaceCount);
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
-
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(value, startDepth);
 };
 
 const stylish = (tree) => {
@@ -43,23 +17,25 @@ const stylish = (tree) => {
     const lines = node.map(({
       key, value, oldValue, status, children,
     }) => {
-      const symbol = findSymbol(status);
-      if (status === 'changed') {
-        if (value === 'nested') {
-          return `${currentIndent}  ${key}: ${iter(children, depth + 1)}`.trimEnd();
-        }
-        const deletedRow = `${currentIndent}- ${key}: ${stringify(oldValue, depth + 1)}`.trimEnd();
-        const addedRow = `${currentIndent}+ ${key}: ${stringify(value, depth + 1)}`.trimEnd();
-        return [deletedRow, addedRow].join('\n');
+      if (status !== 'changed') {
+        const symbol = findSymbol(status);
+        return `${currentIndent}${symbol} ${key}: ${stringify(value, depth + 1)}`.trimEnd();
       }
-      return `${currentIndent}${symbol} ${key}: ${stringify(value, depth + 1)}`.trimEnd();
+      if (value === 'nested') {
+        return `${currentIndent}  ${key}: ${iter(children, depth + 1)}`.trimEnd();
+      }
+      const deletedRow = `${currentIndent}- ${key}: ${stringify(oldValue, depth + 1)}`.trimEnd();
+      const addedRow = `${currentIndent}+ ${key}: ${stringify(value, depth + 1)}`.trimEnd();
+      return [deletedRow, addedRow].join('\n');
     });
+
     return [
       '{',
       ...lines,
       `${bracketIndent}}`,
     ].join('\n');
   };
+
   return iter(tree, 1);
 };
 
--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,31 @@
 export const isObject = (arg) => arg && typeof arg === 'object';
 
 export const has = Object.prototype.hasOwnProperty;
+
+export const stringify = (value, startDepth = 1, spaceCount = 4) => {
+  const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object') {
+      return currentValue.toString();
+    }
+    if (currentValue === undefined) {
+      return 'undefined';
+    }
+    if (currentValue === null) {
+      return 'null';
+    }
+    const indentSize = depth * spaceCount;
+    const currentIndent = ' '.repeat(indentSize);
+    const bracketIndent = ' '.repeat(indentSize - spaceCount);
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(value, startDepth);
+};
